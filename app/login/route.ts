0a115c0972229ebf5842033b052a083a444fba86
Bastien <bastien@alchemy.com>
2023-05-27 04:03:23 +0200

    Add /login route

---
+++ app/login/route.ts
@@ -0,0 +1,25 @@
+export async function GET() {
+  const state = generateRandomString(16);
+  const scope = "user-top-read";
+
+  const searchParams = new URLSearchParams({
+    response_type: "code",
+    client_id: process.env.SPOTIFY_CLIENT_ID || "",
+    scope: scope,
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI || "",
+    state: state,
+  });
+  const redirectUrl = `https://accounts.spotify.com/authorize?${searchParams.toString()}`;
+  return Response.redirect(redirectUrl);
+}
+
+const generateRandomString = function (length: number) {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
