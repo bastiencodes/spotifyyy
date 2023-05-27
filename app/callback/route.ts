5fc1f30781ab45094d74f40a4edff330a616ae38
Bastien <bastien@alchemy.com>
2023-05-27 05:14:14 +0200

    Get access and refresh tokens

--- app/callback/route.ts
+++ app/callback/route.ts
@@ -1,9 +1,43 @@
-export async function GET(request: Request) {
+import { NextRequest, NextResponse } from "next/server";
+import { stateKey } from "../_utils/constants";
+
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
   const state = searchParams.get("state");
-  console.log(code, state);
 
-  const body = JSON.stringify({ code, state });
-  return new Response(body, { status: 200 });
+  const storedState = request.cookies.get(stateKey)?.value;
+
+  if (!code || !state || !storedState || state !== storedState) {
+    return new Response("Invalid state or code or state mismatch", {
+      status: 400,
+    });
+  }
+
+  const clientId = process.env.SPOTIFY_CLIENT_ID || "";
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET || "";
+  const redirectUri = process.env.SPOTIFY_REDIRECT_URI || "";
+
+  const res = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: redirectUri,
+    }),
+  });
+
+  const { access_token, refresh_token } = await res.json();
+
+  const body = JSON.stringify({ access_token, refresh_token });
+
+  const response = new NextResponse(body, { status: 200 });
+
+  response.cookies.delete(stateKey);
+
+  return response;
 }
