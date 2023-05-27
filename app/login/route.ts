69bef6782713bc3366325a99d852efe8121ff0da
Bastien <bastien@alchemy.com>
2023-05-27 04:40:50 +0200

    Set cookie to track generated state

--- app/login/route.ts
+++ app/login/route.ts
@@ -1,28 +1,22 @@
-import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+import { generateRandomString } from "../_utils/generateRandomString";
+import { stateKey } from "../_utils/constants";
 
 export async function GET() {
   const state = generateRandomString(16);
+
   const scope = "user-top-read";
+  const clientId = process.env.SPOTIFY_CLIENT_ID || "";
+  const redirectUri = process.env.SPOTIFY_REDIRECT_URI || "";
 
   const searchParams = new URLSearchParams({
     response_type: "code",
-    client_id: process.env.SPOTIFY_CLIENT_ID || "",
+    client_id: clientId,
     scope: scope,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI || "",
+    redirect_uri: redirectUri,
     state: state,
   });
-
   const redirectUrl = `https://accounts.spotify.com/authorize?${searchParams.toString()}`;
-  redirect(redirectUrl);
+
+  return NextResponse.redirect(redirectUrl).cookies.set(stateKey, state);
 }
-
-const generateRandomString = function (length: number) {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
